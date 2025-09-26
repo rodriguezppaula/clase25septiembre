4ab9866b6de97eb16c98aba6e7cb833a81cbbb8f
Maria Paula <rodriguezppaula@gmail.com>
2025-09-25 21:00:20 -0500

    Ajustes visuales y cálculo del nivel de IMC para mostrar en la alerta

--- formularioIMC/js/script.js
+++ formularioIMC/js/script.js
@@ -1,9 +1,20 @@
 function calcularIMC() {
-    let peso = document.getElementById("peso").value;
-    let estatura = document.getElementById("estatura").value;
+    
+    let peso = parseFloat(document.getElementById("peso").value);
+    let estatura = parseFloat(document.getElementById("estatura").value);
 
-    let division = (peso / (estatura * estatura));
+    let division = (peso / (estatura * estatura)).toFixed(2);
+    let nivel = "";
 
+    if (division < 18.5) {
+        nivel = "Bajo peso";
+    } else if (division >= 18.5 && division <= 24.9) {
+        nivel = "Normal";
+    } else if (division >= 25 && division <= 29.9) {
+        nivel = "Sobrepeso";
+    } else {
+        nivel = "Obesidad";
+    }
 
-    alert("Su índice de masa corporal es: "+ division);
+    alert("Tu índice de masa corporal es: " + division + "\n Y se encuentra en nivel: " + nivel);
 }
